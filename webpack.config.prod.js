c216e1a5be86a29edb81cd50f090316619903771
Fabian Gander <fabian.gander@garaio.com>
2018-07-18 13:23:12 +0200

    Making my own package

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'lib/index'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'react-simple-chatbot.js',
+    filename: 'cyclodex-chatbot.js',
     publicPath: 'dist/',
     library: 'ReactSimpleChatbot',
     libraryTarget: 'umd',
